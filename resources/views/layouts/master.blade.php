520871bd6a10d6af00e165fa5cbd2ed5a0601793
SohanKafle <soka_bca2077@lict.edu.np>
2025-02-13 10:27:41 +0545

    up

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,6 +18,7 @@
         
             <!-- Hamburger Menu for Small Screens -->
             <div class="lg:hidden flex items-center">
+                
                 <button id="menu-toggle" class="text-white hover:text-red-500">
                     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
